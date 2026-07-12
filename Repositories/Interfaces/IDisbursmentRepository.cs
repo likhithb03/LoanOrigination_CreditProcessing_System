0a369342a20e91718e912ffb6734bcc06f90878f
Bhargavthotha <bhargavkali777@gmail.com>
2026-07-12 12:47:32 +0530

    Created IDisbursmentRepository and DisbursmentRepoitory

---
+++ Repositories/Interfaces/IDisbursmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using LOCPS.Models;
+
+namespace LOCPS.Repositories.Interfaces
+{
+    public interface IDisbursmentRepository : IGenericRepository<Disbursment>
+    {
+        Task<Disbursment> CreateDisbursmentAsync(Disbursment disbursment);
+        Task<Disbursment?> GetDisbursmentByApplicationIdAsync(int applicationId);
+        Task<Disbursment> UpdateDisbursmentAsync(Disbursment disbursment);
+        Task<IEnumerable<Disbursment>> GetPendingDisbursmentsAsync(DisbursmentStatus status);
+
+    }
+}
