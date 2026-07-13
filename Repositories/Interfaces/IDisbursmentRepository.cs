ef72cc97d1a14664cded3da1f7b9bc1e530885e2
Bhargavthotha <bhargavkali777@gmail.com>
2026-07-13 14:01:49 +0530

    Adding IDisbisementRepository by Bhargav

--- Repositories/Interfaces/IDisbursmentRepository.cs
+++ Repositories/Interfaces/IDisbursmentRepository.cs
@@ -4,10 +4,10 @@ namespace LOCPS.Repositories.Interfaces
 {
     public interface IDisbursmentRepository : IGenericRepository<Disbursment>
     {
-        Task<Disbursment> CreateDisbursmentAsync(Disbursment disbursment);
-        Task<Disbursment?> GetDisbursmentByApplicationIdAsync(int applicationId);
-        Task<Disbursment> UpdateDisbursmentAsync(Disbursment disbursment);
-        Task<IEnumerable<Disbursment>> GetPendingDisbursmentsAsync(DisbursmentStatus status);
+        Task<Disbursment> CreateDisbursmentAsync(Disbursment disbursment); // create a new Disbuesment record
+        Task<Disbursment?> GetDisbursmentByApplicationIdAsync(int applicationId); //Finds the disbursement associated with a loan application
+        Task<Disbursment> UpdateDisbursmentAsync(Disbursment disbursment);//Updates an existing disbursement
+        Task<IEnumerable<Disbursment>> GetPendingDisbursmentsAsync(DisbursmentStatus status); //Retrieves disbursements having a specific status.
 
     }
 }
