dfc513bb5aefcf22279cee5d91bd2d7c2feecc34
Mudi <2506830@cognizant.com>
2026-07-14 15:30:52 +0530

    Simple

---
+++ Migrations/20260714091336_SSMS_PUSH.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LOCPS.Migrations
+{
+    /// <inheritdoc />
+    public partial class SSMS_PUSH : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Approval_Approval_ApprovalStatusApprovalId",
+                table: "Approval");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Approval_ApprovalStatusApprovalId",
+                table: "Approval");
+
+            migrationBuilder.RenameColumn(
+                name: "ApprovalStatusApprovalId",
+                table: "Approval",
+                newName: "ApprovalStatus");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ApprovalStatus",
+                table: "Approval",
+                newName: "ApprovalStatusApprovalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Approval_ApprovalStatusApprovalId",
+                table: "Approval",
+                column: "ApprovalStatusApprovalId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Approval_Approval_ApprovalStatusApprovalId",
+                table: "Approval",
+                column: "ApprovalStatusApprovalId",
+                principalTable: "Approval",
+                principalColumn: "ApprovalId");
+        }
+    }
+}
